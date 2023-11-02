2ecf0ffa22bcc77614d1d914900895b0c867d1ad
Zeds_Xd <homenkor24@gmail.com>
2023-11-02 10:12:53 +0300

    Add new Script

---
+++ Scripts/SpawnerGameObject/Spawner.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private Transform[] _spawnPoints;
+    [SerializeField] private float _secondsBetweenSpawn;
+
+    private float _elapsedTime = 0;
+
+    private void Update()
+    {
+        _elapsedTime += Time.deltaTime;
+
+        if (_elapsedTime >= _secondsBetweenSpawn)
+        {
+            _elapsedTime = 0;
+
+            int spawnPointNumber = Random.Range(0, _spawnPoints.Length);
+            Instantiate(_enemyPrefab, _spawnPoints[spawnPointNumber]); 
+        }
+    }
+}
